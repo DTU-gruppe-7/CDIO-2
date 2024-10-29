91db4dd87d375f820ef7c492e7955e74531853b9
Johan Theis Møller <s245513@student.dtu.dk>
2024-10-29 16:15:20 +0100

    The methods for congratulating winner and starting the game is added to the DiceGame class

--- DiceGame.java
+++ DiceGame.java
@@ -1,5 +1,22 @@
-class DiceGame{
-    static void main(String[] args){
+import java.util.Scanner;
+class DiceGame {
+    private static Player player1;
+    private static Player player2;
+    private static Scanner s;
+    public static void main (String[]args){
+        s = new Scanner(System.in);
 
+    }
+    private void startGame(){
+        //get player names from user
+        System.out.println("The game has begun. Buckle up!")
+        System.out.println("Please input the username of player 1");
+        player1=new Player(s.nextline());
+        System.out.println("Please input the username of player 2");
+        player2=new Player(s.nextline());
+    }
+
+    private void congratulateWinner(Player player){
+        System.out.println("Congratulations " + player.getName() + "you won and are now rich :D");
     }
 }
