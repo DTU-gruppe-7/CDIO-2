a43ad9781cf88ec861c591ef285f3bf9e6a23743
Mathias Rasmussen <s235447@student.dtu.dk>
2024-10-29 15:56:08 +0100

    Added the field for the faceValue and the function and the function getFaceValue

--- Dice.java
+++ Dice.java
@@ -1,3 +1,10 @@
 class Dice {
-    
+    private int faceValue;
+
+    public void roll() {
+       this.faceValue = (int)(Math.random() * 6) + 1;
+    }
+    public int getFaceValue(){
+            return faceValue;
+        }
 }
