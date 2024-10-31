d4f854c04c8e810a079bd0848b9e3129e54b91fb
JeppeRohde <s246221@student.dtu.dk>
2024-10-31 23:21:50 +0100

    Added comments

--- DiceGame.java
+++ DiceGame.java
@@ -1,18 +1,20 @@
+//Main class directing the game and player interactions
+
 import java.util.Scanner;
 class DiceGame {
-    private static Player player1;
+    private static Player player1; //Field to store the players
     private static Player player2;
     private static Scanner s;
     public static void main (String[]args){
         s = new Scanner(System.in);
-        startGame();
-        while (true) {
-            player1.account.resetBalance();
+        startGame(); //Calls method that starts the game
+        while (true) { //This inner/outer loop-method is suitable for allowing rematches
+            player1.account.resetBalance(); //Resets score to 1000
             player2.account.resetBalance();
-            while (true) {
-                playRound(player1);
-                if (player1.account.getBalance() >= 3000){
-                    congratulateWinner(player1);
+            while (true) { //Representing each round
+                playRound(player1); //Calls method that rolls the dice
+                if (player1.account.getBalance() >= 3000){ //Ensures if players has won
+                    congratulateWinner(player1); // Calls method to declare winner
                     break;
                 }
                 playRound(player2);
@@ -32,8 +34,7 @@ class DiceGame {
         s.close();
     }
 
-    private static void startGame(){
-        //get player names from user
+    private static void startGame(){ //Gets player names from user
         System.out.println("The game has begun. Buckle up!");
         System.out.println("Please input the username of player 1");
         player1=new Player(s.nextLine());
@@ -41,11 +42,11 @@ class DiceGame {
         player2=new Player(s.nextLine());
     }
 
-    private static void congratulateWinner(Player player){
+    private static void congratulateWinner(Player player){ //Method calling class displaying the winner
         System.out.println("Congratulations " + player.getName() + " you won and are now rich :D");
     }
 
-    private static void playRound(Player player){
+    private static void playRound(Player player){ //Method calling class to roll two dice
         Dice dice1=new Dice();
         Dice dice2=new Dice();
 
@@ -53,22 +54,23 @@ class DiceGame {
             System.out.println(player.getName() + " press enter to roll");
             s.nextLine();
 
-            dice1.roll();
-            dice2.roll();
+            dice1.roll(); //Rolls first dice
+            dice2.roll(); //Rolls second dice
 
             System.out.println("You rolled " + dice1.getFaceValue() + " with your first dice");
             System.out.println("You rolled " + dice2.getFaceValue() + " with your second dice");
 
-            int diceSum = dice1.getFaceValue() + dice2.getFaceValue();
-            int fieldValue = Board.getField(diceSum);
+            int diceSum = dice1.getFaceValue() + dice2.getFaceValue(); //Calculates sum of dice
+            int fieldValue = Board.getField(diceSum); //Retrieves field values
 
             System.out.println(Board.toString(diceSum));
 
-            if (diceSum == 10) {
+            if (diceSum == 10) { //This function adjusts player's balance based on the rules given by TA
                 if (!player.account.withdraw(fieldValue)){
                     System.out.println("Your balance is too low so the withdraw failed");
                 }
-                System.out.println(player.getName() + " your temp balance is now: " + player.account.getBalance() + System.lineSeparator()); 
+                System.out.println(player.getName() + " your temp balance is now: " 
+                + player.account.getBalance() + System.lineSeparator()); 
             } else if (fieldValue < 0){
                 if(!player.account.withdraw(fieldValue)) {
                     System.out.println("Your balance is too low so the withdraw failed");
@@ -81,6 +83,7 @@ class DiceGame {
             } 
             
         } while (true);
-        System.out.println(player.getName() + " your balance is now: " + player.account.getBalance() + System.lineSeparator());
+        System.out.println(player.getName() + " your balance is now: " +
+        player.account.getBalance() + System.lineSeparator()); //Calls method that lets the player see current balance
     }
 }
