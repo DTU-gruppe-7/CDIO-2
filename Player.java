59ee8cb6eb5ad3454337eca504172b73454770f6
krille2807 <s246105@dtu.dk>
2024-10-31 10:07:19 +0100

    updated player class

--- Player.java
+++ Player.java
@@ -1,3 +1,26 @@
 public class Player {
+    private String name;
+    private Account account;
     
+    public Player(String name, Account account) {
+        this.name = name;
+        this.account = new Account();
+
+    }
+
+    public String getName(){
+        return name;
+    }  
+
+    public boolean deposit(int amount) {
+        return account.deposit(amount);
+    }
+
+    public boolean withdraw(int amount) {
+        return account.withdraw(amount);
+    }
+
+    public int getBalance() {
+        return account.getBalance();
+    }
 }
